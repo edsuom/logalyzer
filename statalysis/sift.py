a05178792497019f6615fed80969d7cac4f2b653
edsuom <edsuom@a3b7badd-e2cd-4207-ba89-9dbf7d88ca34>
2015-02-24 22:20:07 +0000

    Got User-Agent matcher working and tested
    git-svn-id: svn+ssh://tellectual.com/svn/devel@274 a3b7badd-e2cd-4207-ba89-9dbf7d88ca34

--- statalysis/sift.py
+++ statalysis/sift.py
@@ -17,14 +17,46 @@ class MatcherBase(object):
     """
     Build your matcher on me
     """
-    N_cache = 20
+    def newCache(self, N=30):
+        """
+        Generates the FIFO queue for a new sort-of LRU cache of strings
+        and returns its index, starting with 0 for the first cache.
+        """
+        if not hasattr(self, 'caches'):
+            self.caches = []
+        thisCache = deque([""], N)
+        self.caches.append(thisCache)
+        return len(self.caches) - 1
+
+    def clearCache(self):
+        for cache in self.caches:
+            cache.clear()
     
+    def checkCache(self, k, x):
+        """
+        Checks cache k for the string x, returning True if it's there or
+        False if not.
+        """
+        return bool(self.caches[k].count(x))
+
+    def setCache(self, k, x):
+        """
+        Appends x to cache k, which will result in it being found there if
+        checked within N cache misses.
+
+        The value least recently added (from a cache miss) will be
+        popped off the other end. It isn't strictly an LRU cache,
+        since a cache hit will be drowned in misses.
+        """
+        self.caches[k].append(x)
+
     def fileLinerator(self, filePath):
         fh = open(filePath, 'rb')
         for line in fh:
-            if line.startswith("#"):
+            line = line.strip()
+            if not line or line.startswith("#"):
                 continue
-            yield line.strip()
+            yield line
         fh.close()
 
 
@@ -36,14 +68,15 @@ class IPMatcher(MatcherBase):
     
     def __init__(self):
         self.networks = []
-        self.longs = array.array('L')
-        self.clearCache()
-
-    def clearCache(self):
-        self.cachedTrue = array.array('L')
-        self.cachedFalse = deque(
-            array.array('L', [0]*self.N_cache), self.N_cache)
-        
+        # Cache for Offenders
+        self.newCache()
+        # Cache for innocents
+        self.newCache()
+        # LongInt lookup table for making it faster to add rules.
+        self.netLongs = array.array('L')
+        # LongInt lookup table for known and added offenders
+        self.ipLongs = array.array('L')
+    
     def addRules(self, filePath):
         """
         Add some rules from a text file with lines in aaa.bbb.ccc.ddd/ee notation
@@ -55,37 +88,51 @@ class IPMatcher(MatcherBase):
             thisNet = ipcalc.Network(match.group(0))
             # Quick check
             thisLong = thisNet.network_long()
-            if thisLong in self.longs:
+            if thisLong in self.netLongs:
                 # Same long network address, so do more thorough check
                 for otherNet in self.networks:
                     if thisNet.check_collision(otherNet):
                         # Yep, redundant rule
                         break
                 else:
-                    # No collisions, so actually not a redundant rule; add it
-                    # (Will this ever happen with properly defined rules?)
+                    # No collision, so actually NOT a redundant rule;
+                    # add it. (Will this ever happen with properly
+                    # defined rules?)
                     self.networks.append(thisNet)
             else:
                 # New long network address, add both it and the network object
-                self.longs.append(thisLong)
+                self.netLongs.append(thisLong)
                 self.networks.append(thisNet)
 
+    def addOffender(self, ip):
+        """
+        Call this with an IP address (string format) to add it to my list
+        of offenders.
+        """
+        self.ipLongs.append(long(ipcalc.IP(ip)))
+                
     def __call__(self, ip):
-        net = ipcalc.Network(ip)
-        netLong = net.network_long()
-        # Likely to be many multiple hits from offenders, much faster
-        # to check for previously matched long
-        if netLong in self.cachedTrue:
+        # Likely to be several sequential hits from offenders and
+        # innocents alike
+        if self.checkCache(0, ip):
+            # Offender was cached
             return True
-        # Also expect repeated checks of regular IPs, though temporally limited
-        if self.cachedFalse.count(netLong):
+        if self.checkCache(1, ip):
+            # Innocent was cached
             return False
+        ipObject = ipcalc.IP(ip)
+        # Check for known and added offenders
+        ipLong = long(ipObject)
+        if ipLong in self.ipLongs:
+            return True
         # Not found (yet), go through the actual list of networks
-        for other in self.networks:
-            if net.check_collision(other):
-                self.cachedTrue.append(netLong)
+        for net in self.networks:
+            if net.has_key(ipObject):
+                # Offender found
+                self.setCache(0, ip)
+                self.ipLongs.append(ipLong)
                 return True
-        self.cachedFalse.append(netLong)
+        self.setCache(1, ip)
         return False
 
 
@@ -94,21 +141,31 @@ class UAMatcher(MatcherBase):
     I efficiently match User Agent strings with regular expressions
     """
     def __init__(self, uaFilePath):
-        self.lastUA, self.lastResult = "", False
-        self.reParts = []
-        for line in self.fileLinerator(filePath):
-            self.reParts.append(line)
-            
-    def __call__(self, uaString):
-        # Compiled RE should be fast enough that only the most
-        # rudimentary caching makes sense
-        if uaString == self.lastUA:
-            return self.lastResult
-        self.lastUA = uaString
-        if not hasattr(self, 'reUA'):
-            self.reUA = re.compile(r'|'.join(self.reParts))
-        self.lastResult = bool(self.reUA.search(uaString))
-        return self.lastResult
+        # Cache for Offenders
+        self.newCache()
+        # Cache for innocents
+        self.newCache()
+        reParts = []
+        for line in self.fileLinerator(uaFilePath):
+            reParts.append(line)
+        self.reUA = re.compile(r'|'.join(reParts))
+    
+    def __call__(self, ip, uaString):
+        # Likely to be several sequential hits from offenders and
+        # innocents alike
+        if self.checkCache(0, ip):
+            # Offender was cached
+            return True
+        if self.checkCache(1, ip):
+            # Innocent was cached
+            return False
+        if self.reUA.search(uaString):
+            # Offender found
+            self.setCache(0, ip)
+            return True
+        self.setCache(1, ip)
+        return False
+
     
                     
             
--- statalysis/test_sift.py
+++ statalysis/test_sift.py
@@ -7,7 +7,7 @@ Copyright (C) 2014-2015 Tellectual LLC
 
 """
 
-import os.path
+import os.path, random
 
 import ipcalc
 
@@ -45,11 +45,60 @@ class TestIPMatcher(tb.TestCase):
         for thisIP, expectMatch in cases:
             self.assertEqual(self.ip(thisIP), False)
         # Load rules, clear cache, and try again
-        self.ip.clearCache()
         self.ip.addRules(self.rulePath)
+        self.ip.clearCache()
         for thisIP, expectMatch in cases:
             self.assertEqual(self.ip(thisIP), expectMatch, thisIP)
         
 
-        
+class TestUAMatcher(tb.TestCase):
+    def setUp(self):
+        filePath = os.path.join(
+            tb.moduleDir(parent=True), "stuff", "ua.txt")
+        self.ua = sift.UAMatcher(filePath)
+        self.table = {}
 
+    def checkWithRandomIP(self, uaString, expected):
+        def msg():
+            maybeNo = "" if expected else "no "
+            return "Expected {}match for {}: '{}'".format(
+                maybeNo, ip, uaString)
+        
+        thisList = self.table.setdefault(expected, [])
+        while len(thisList) < 10:
+            thisList.append(
+                ".".join([str(random.randint(0,255)) for k in xrange(4)]))
+        ip = random.choice(thisList)
+        if expected:
+            self.assertTrue(self.ua(ip, uaString), msg())
+        else:
+            self.assertFalse(self.ua(ip, uaString), msg())
+        
+    def test_someMatches(self):
+        negatives = (
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:20.0) Gecko/20100101 Firefox/20.0",
+            "Mozilla/5.0 (compatible; Google Desktop/5.9.1005.12335; http://desktop.google.com/)",
+            "Mozilla/5.0 (iPad; CPU OS 6_0_1 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A523 Safari/8536.25",
+            "Perfect%20Browser-iPad/7.0 CFNetwork/609 Darwin/13.0.0",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:19.0) Gecko/20100101 Firefox/19.0",
+            "Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Mobile/10A403 [FBAN/FBIOS;FBAV/5.3;FBBV/89182;FBDV/iPhone4,1;FBMD/iPhone;FBSN/iPhone OS;FBSV/6.0;FBSS/2; FBCR/AT&T;FBID/phone;FBLC/en_US]",
+            )
+        positives = (
+            "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+            "facebookexternalhit/1.1 (+http://www.facebook.com/externalhit_uatext.php)",
+            "Mozilla/5.0 (compatible; YandexBot/3.0; +http://yandex.com/bots)",
+            "Mozilla/5.0 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)",
+            "msnbot/2.0b (+http://search.msn.com/msnbot.htm)",
+            "Twitterbot/1.0",
+            "Mozilla/5.0 (compatible; DotBot/1.1; http://www.opensiteexplorer.org/dotbot, help@moz.com)",
+            "adbeat_bot",
+            "Java/1.6.0_26",
+            )
+        # Test negatives and positives, twice each to ensure caching doesn't screw things up
+        for k in xrange(10):
+            for uaString in negatives:
+                self.checkWithRandomIP(uaString, False)
+            for uaString in positives:
+                self.checkWithRandomIP(uaString, True)
+
+        
