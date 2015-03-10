716d017361c58770b6e08597d07f301337a89bad
edsuom <edsuom@a3b7badd-e2cd-4207-ba89-9dbf7d88ca34>
2015-03-10 04:17:02 +0000

    Finished initial untried and untested write of urwid gui
    git-svn-id: svn+ssh://tellectual.com/svn/devel@321 a3b7badd-e2cd-4207-ba89-9dbf7d88ca34

--- statalysis/gui.py
+++ statalysis/gui.py
@@ -7,64 +7,95 @@ Copyright (C) 2014-2015 Tellectual LLC
 
 """
 
-import urwid
+import urwid as u
 
 
-class Main(urwid.WidgetWrap):
+class MessageBox(u.Frame):
     """
-    I am the main curses interface.
+    I am a message box consisting of a heading and an expandable space
+    for lines you can add under the heading.
     """
-    self.header = Messages()
-    self.body = FileListBox()
-    
-    
-    
-    def msgHeading(self, text):
-        pass
+    def __init__(self, text):
+        header = u.Text(('heading', text))
+        self.w = u.SimpleListWalker([])
+        body = u.ListBox(self.w)
+        super(Message, self).__init__(body, header=header)
 
-    def msgBody(self, text):
-        pass
+    def add(self, text):
+        self.w.append(u.Text(text))
+                            
+
+class Messages(u.ListBox):
+    """
+    I provide a place for status messages to be displayed. A heading
+    message will be set off from anything above it, and you can supply
+    an ID for it. A body message will added below whatever message was
+    last supplied, or below the last body message for a particular
+    heading ID if you specify an ID and one with that ID was received.
+    """
+    def __init__(self):
+        self.boxes = {}
+        body = u.SimpleListWalker([])
+        super(Messages, self).__init__(body)
         
+    def heading(self, text, ID=None):
+        msg = MessageBox(text)
+        self.boxes[ID] = msg
+
+    def body(self, text, ID):
+        thisBox = self.boxes[ID]
+        thisBox.add(text)
+        # Do I need to explicitly set focus now?
 
 
-class Messages(urwid.Text):
-    """
-    I provide a place for status messages to be displayed
-    """
-    def __init__(self, rows):
-        super(Messages, self).__init__(align='left', wrap='clip')
-        self.newHeight(rows)
-
-    def newLines(self, *lines):
-        self.lines.append(line)
-        while len(self.lines) > self.rows:
-            self.lines.pop(0)
-        self.updateText()
-        
-    def newHeading(self, text):
-        # TODO: Header formatting
-        self.newLines("", text)
-
-    def newBody(self, text):
-        self.newLines(text)
-
-    def newHeight(self, rows):
-        self.rows = rows
-        oldLines = getattr(self, 'lines', [])
-        self.lines = oldLines[:rows]
-        self.updateText()
-        
-    def updateText(self):
-        text = u"\n".join(self.lines)
-        self.set_text(text)
-    
-
-class FileListBox(urwid.ListBox):
+class Files(u.ListBox):
     """
     I occupy most of the screen with a list of access log files being
     processed.
     """
     def __init__(self):
-        pass
+        self.rows = {}
+        self.w = u.SimpleListWalker([])
+        super(Files, self).__init__(self.w)
+
+    def setStatus(self, fileName, text):
+        row = self.rows.get(fileName, None)
+        if row is None:
+            statusBox = u.Text("", wrap='clip')
+            widgets = [u.Text(fileName), statusBox]
+            row = u.Columns([widgets], dividechars=1)
+            self.rows[fileName] = row
+            self.w.append(row)
+        else:
+            statusBox = row.contents[1][0]
+        statusBox.set_text(text)
 
     
+class GUI(object):
+    """
+    I am the main curses interface.
+    """
+    palette = [('heading', 'default,bold', 'default'),]
+
+    def __init__(self):
+        self.id_counter = 0
+        self.messages = u.LineBox(Messages())
+        self.files = u.LineBox(Files())
+        self.main = u.WidgetWrap(u.Pile([self.messages, self.files]))
+    
+    def msgHeading(self, text, ID=None):
+        if ID is None:
+            self.id_counter += 1
+            ID = self.id_counter
+        self.messages.heading(text, ID)
+        return ID
+
+    def msgBody(self, text, ID=None):
+        if ID is None:
+            ID = self.id_counter
+        self.messages.body(text, ID)
+
+    def fileStatus(self, fileName, text):
+        self.files.setStatus(fileName, text)
+
+        
