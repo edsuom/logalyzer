902c4bfc237941048599545bad536ddafd8f4695
edsuom <edsuom@a3b7badd-e2cd-4207-ba89-9dbf7d88ca34>
2015-03-10 23:46:34 +0000

    Unittested MessageBox and Messages, prety cool
    git-svn-id: svn+ssh://tellectual.com/svn/devel@325 a3b7badd-e2cd-4207-ba89-9dbf7d88ca34

--- statalysis/gui.py
+++ statalysis/gui.py
@@ -10,91 +10,154 @@ Copyright (C) 2014-2015 Tellectual LLC
 import urwid as u
 
 
-class MessageBox(u.Frame):
+class MessageBox(u.ListBox):
     """
     I am a message box consisting of a heading and an expandable space
     for lines you can add under the heading.
     """
     def __init__(self, text):
-        header = u.Text(('heading', text))
-        self.w = u.SimpleListWalker([])
-        body = u.ListBox(self.w)
-        super(MessageBox, self).__init__(body, header=header)
+        self.hText = text
+        body = u.SimpleListWalker([self.headingText(True)])
+        super(MessageBox, self).__init__(body)
 
+    def headingText(self, isCurrent):
+        attrName = "heading"
+        if isCurrent:
+            attrName += "_current"
+        return u.Text((attrName, self.hText))
+
+    def setCurrent(self, yes):
+        self.body[0] = self.headingText(yes)
+        if yes:
+            self.body.set_focus(0)        
+    
     def add(self, text):
-        self.w.append(u.Text(text))
+        self.body.append(u.Text(text))
+        self.setCurrent(True)
+        return len(self.body)
                             
 
 class Messages(u.ListBox):
     """
-    I provide a place for status messages to be displayed. A heading
-    message will be set off from anything above it, and you can supply
-    an ID for it. A body message will added below whatever message was
-    last supplied, or below the last body message for a particular
-    heading ID if you specify an ID and one with that ID was received.
+    I provide a place for status messages to be displayed below headings.
+
+    A heading will be set off from anything above it, and provides an
+    anchor for messages. Each message will be appended below the
+    heading with its ID.
+
     """
+    defaultHeight = 3
+    
     def __init__(self):
-        self.boxes = {}
+        self.boxes = []
         body = u.SimpleListWalker([])
         super(Messages, self).__init__(body)
+
+    def adapt(self, w, height=None):
+        if height is None:
+            height = self.defaultHeight
+        return u.BoxAdapter(w, height)
         
-    def heading(self, text, ID=None):
-        msg = MessageBox(text)
-        self.boxes[ID] = msg
+    def heading(self, text, ID):
+        if ID in self.boxes:
+            raise IndexError("ID '{}' already exists".format(ID))
+        if len(self.boxes):
+            text = "\n" + text
+        self.boxes.append(ID)
+        msgBox = self.adapt(MessageBox(text))
+        self.body.append(msgBox)
 
-    def body(self, text, ID):
-        thisBox = self.boxes[ID]
-        thisBox.add(text)
-        # Do I need to explicitly set focus now?
+    def msg(self, text, ID):
+        if ID not in self.boxes:
+            raise IndexError(
+                "No heading for ID '{}'".format(ID))
+        k = self.boxes.index(ID)
+        # De-emphasize any other boxes
+        for kk, msgBox in enumerate(self.body):
+            if kk != k:
+                msgBox.setCurrent(False)
+        # Add the message line under the heading
+        newHeight = self.body[k].add(text)
+        if newHeight > self.defaultHeight:
+            rawBox = self.body[k].original_widget
+            self.body[k] = self.adapt(rawBox, newHeight)
+        # Set focus to this last-updated message
+        self.body.set_focus(k)
 
 
-class Files(u.ListBox):
+class Files(u.Pile):
     """
     I occupy most of the screen with a list of access log files being
     processed.
     """
-    def __init__(self):
-        self.rows = {}
-        self.w = u.SimpleListWalker([])
-        super(Files, self).__init__(self.w)
+    gutterWidth = 2
+    
+    def __init__(self, fileNames):
+        self.fileNames = fileNames
+        self.leftColWidth = max([len(x) for x in fileNames])
+        widgetList = [(1, self._makeRow(x)) for x in fileNames]
+        super(Files, self).__init__(widgetList)
 
+    def _statusBox(self, text):
+        return u.Text("", wrap='clip')
+        
+    def _makeRow(self, fileName):
+        widgetList = [
+            (self.leftColWidth, u.Text(fileName, align='right')),
+            self._statusBox("")]
+        return u.Columns(widgetList, dividechars=self.gutterWidth)
+        
     def setStatus(self, fileName, text):
-        row = self.rows.get(fileName, None)
-        if row is None:
-            statusBox = u.Text("", wrap='clip')
-            widgets = [u.Text(fileName), statusBox]
-            row = u.Columns([widgets], dividechars=1)
-            self.rows[fileName] = row
-            self.w.append(row)
-        else:
-            statusBox = row.contents[1][0]
-        statusBox.set_text(text)
+        if fileName not in self.fileNames:
+            raise IndexError("Unknown filename '{}'".format(fileName))
+        k = self.fileNames.index(fileName)
+        rowWidget = self.contents[k][0]
+        rowWidget[1].set_text(text)
 
     
 class GUI(object):
     """
     I am the main curses interface.
     """
-    palette = [('heading', 'default,bold', 'default'),]
+    palette = [
+        # Name
+        # 'fg color,setting', 'background color', 'mono setting'
+        ('heading',
+         'dark cyan', 'default', 'bold'),
+        ('heading_current',
+         'yellow,bold', 'default', 'bold'),
+    ]
 
-    def __init__(self):
+    def __init__(self, fileNames):
         self.id_counter = 0
-        self.messages = u.LineBox(Messages())
-        self.files = u.LineBox(Files())
-        main = u.WidgetWrap(u.Pile([self.messages, self.files]))
-        eventLoop = u.TwistedEventLoop()
+        self.m = Messages()
+        self.f = Files(fileNames)
+        self.pile = u.Pile([
+            self.m, u.Divider('=', 1, 1), (len(fileNames), self.f)])
+        main = u.WidgetWrap(u.LineBox(self.pile))
+        eventLoop = u.TwistedEventLoop(reactor, manage_reactor=False)
+        self.screen = Screen()
+        self.screen.register_palette(self.palette)
+        self.screen.set_terminal_properties(
+            colors=16,
+            bright_is_bold=True)
         self.loop = u.MainLoop(
-            main, palette=self.palette, handle_mouse=False,
-            unhandled_input=eventLoop(self.possiblyQuit),
-            event_loop=eventLoop)
+            main, screen=self.screen,
+            unhandled_input=self.possiblyQuit, event_loop=eventLoop)
+        reactor.addSystemEventTrigger('before', 'shutdown', self.stop)
+        self.loop.start()
 
-    def run(self):
-        self.loop.run()
+    def update(self):
+        self.loop.draw_screen()
+
+    def stop(self):
+        self.screen.unhook_event_loop(self.loop)
+        self.loop.stop()
 
     def possiblyQuit(self, key):
         if key in ('q', 'Q'):
-            raise u.ExitMainLoop()
-        
+            reactor.stop()
+    
     def msgHeading(self, text, ID=None):
         if ID is None:
             self.id_counter += 1
--- statalysis/test_gui.py
+++ statalysis/test_gui.py
@@ -7,12 +7,25 @@ Copyright (C) 2014-2015 Tellectual LLC
 
 """
 
+import random
+
 import urwid as u
+from urwid.raw_display import Screen
+
 from twisted.internet import defer, reactor
 
-from testbase import *
+import testbase as tb
 
 import gui
+
+
+LINES = """
+Once there was a man from Devrizes
+who had balls of two different sizes.
+The one was so small
+it was no ball at all.
+But the other one several prizes.
+""".strip().split('\n')
 
 
 def deferToDelay(delay=5):
@@ -24,33 +37,138 @@ def deferToDelay(delay=5):
 class Display(object):
     lifetime = 10 # seconds
     
-    palette = [('heading', 'default,bold', 'default'),]
-    
     def __init__(self, widget):
-        eventLoop = u.TwistedEventLoop(reactor, manage_reactor=False)
-
-        @eventLoop.handle_exit
         def possiblyQuit(key):
             if key in ('q', 'Q'):
-                raise u.ExitMainLoop()
-        
-        main = u.WidgetWrap(widget)
-        #reactor.callLater(self.lifetime, possiblyQuit, 'q')
-        reactor.addSystemEventTrigger('before', 'shutdown', self.stop)
+                reactor.stop()
+
+        main = u.WidgetWrap(u.LineBox(widget))
+        eventLoop = u.TwistedEventLoop(reactor, manage_reactor=False)
+        self.screen = Screen()
+        self.screen.register_palette(gui.GUI.palette)
+        self.screen.set_terminal_properties(
+            colors=16,
+            bright_is_bold=True)
         self.loop = u.MainLoop(
-            main, palette=self.palette, handle_mouse=False,
-            unhandled_input=possiblyQuit, event_loop=eventLoop)
+            main, screen=self.screen,
+            unhandled_input=possiblyQuit,
+            event_loop=eventLoop)
+        reactor.addSystemEventTrigger('before', 'shutdown', self.stop)
         self.loop.start()
 
-    def stop(self):
-        self.loop.stop()
-        
+    def update(self):
+        self.loop.draw_screen()
 
-class TestMessages(TestCase):
+    def stop(self):
+        self.screen.unhook_event_loop(self.loop)
+        self.loop.stop()
+
+        
+class TestCase(tb.TestCase):
     def setUp(self):
+        if hasattr(self, 'wInstance'):
+            self.w = self.wInstance()
+        else:
+            self.w = self.wClass(*getattr(self, 'wArgs', []))
+        self.display = Display(self.w)
+
+    def tearDown(self):
+        self.display.stop()
+        
+    def showBriefly(self, delay=0.5):
+        self.display.update()
+        return deferToDelay(delay)
+
+
+class TestMessageBox(TestCase):
+    wClass = gui.MessageBox
+    wArgs = ["This is a heading!"]
+
+    @defer.inlineCallbacks
+    def test_add(self):
+        yield self.showBriefly()
+        for line in LINES:
+            self.w.add(line)
+            yield self.showBriefly()
+
+    @defer.inlineCallbacks
+    def test_toggleCurrent(self):
+        for k in xrange(5):
+            isCurrent = bool(k % 2)
+            if isCurrent:
+                line = "Current, emphasize!"
+            else:
+                line = "Not current, pay no attention."
+            self.w.add(line)
+            self.w.setCurrent(isCurrent)
+            yield self.showBriefly(1.0)
+            
+        
+class TestMessages(TestCase):
+    wClass = gui.Messages
+
+    @defer.inlineCallbacks
+    def test_justOne(self):
+        self.w.heading("First Heading", 1)
+        yield self.showBriefly()
+        for line in LINES:
+            self.w.msg(line, 1)
+            yield self.showBriefly()
+
+    @defer.inlineCallbacks
+    def test_several(self):
+        for k in xrange(1,6):
+            self.w.heading("Heading #{:d}".format(k), k)
+            yield self.showBriefly()
+            for line in LINES:
+                self.w.msg(line, k)
+                yield self.showBriefly(0.2)
+
+    @defer.inlineCallbacks
+    def test_updateHeadings(self):
+        IDs = ['Alpha', 'Bravo', 'Charlie', 'Delta']
+        for ID in IDs:
+            self.w.heading("Heading {}".format(ID), ID)
+            yield self.showBriefly(0.1)
+        for k in xrange(20):
+            ID = random.choice(IDs)
+            line = random.choice(LINES)
+            self.w.msg(line, ID)
+            yield self.showBriefly(0.2)
+
+
+class TestMessagesWithFiller(TestCase):
+    def wInstance(self):
         self.m = gui.Messages()
-        self.display = Display(self.m)
-    
-    def test_justHeading(self):
-        self.m.heading("Foo Bar", 1)
-        return deferToDelay()
+        return u.Pile([
+            self.m, (2, u.SolidFill('/'))])
+
+    @defer.inlineCallbacks
+    def test_fits(self):
+        rows = self.display.screen.get_cols_rows()[1]
+        self.w.contents[1] = (self.w.contents[1][0], ('given', rows/2))
+        for k in xrange(1,3):
+            self.m.heading("Heading #{:d}".format(k), k)
+            for line in LINES:
+                self.m.msg(line, k)
+                yield self.showBriefly(0.1)
+        yield self.showBriefly(1.0)
+
+    @defer.inlineCallbacks
+    def test_dynamicResizing(self):
+        rowsBefore = 0
+        for step in xrange(100):
+            rows = self.display.screen.get_cols_rows()[1]
+            if rows != rowsBefore:
+                self.w.contents[1] = (
+                    self.w.contents[1][0], ('given', rows/2))
+                self.m.heading(
+                    "Changed from {:d} to {:d} rows".format(
+                        rowsBefore, rows), step)
+                rowsBefore = rows
+                self.m.msg("Adjust window size to see another update", step)
+            yield self.showBriefly(0.1)
+
+            
+        
+                
