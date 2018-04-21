ef406ab5a928b01790417561e0009825c80b6c22
Ed Suominen <valueprivacy-foss@yahoo.com>
2018-04-20 18:35:02 -0700

    Preparing for initial public release

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 #
 # logalyzer:
-# 
-# Asynchronous task queueing based on the Twisted framework, with task
-# prioritization and a powerful worker interface.
+# Parses your bloated HTTP access logs to extract the info you want
+# about hits to your webserver from (hopefully) real people instead of
+# just the endless hackers and bots. Stores the info in a relational
+# database where you can access it using all the power of SQL.
 #
-# Copyright (C) 2006-2007, 2015 by Edwin A. Suominen,
-# http://edsuom.com/AsynQueue
+# Copyright (C) 2015, 2017, 2018 by Edwin A. Suominen,
+# http://edsuom.com/logalyzer
 #
 # See edsuom.com for API documentation as well as information about
 # Ed's background and other projects, software and otherwise.
@@ -34,31 +35,42 @@ required = ['sAsync']
 
 
 ### Define setup options
-kw = {'version':'0.4',
-      'license':'Apache License (2.0)',
-      'platforms':'OS Independent',
+kw = {'version':           "0.4",
+      'license':           "Apache License (2.0)",
+      'platforms':         "OS Independent",
 
-      'url':"http://edsuom.com/{}.html".format(NAME),
-      'author':"Edwin A. Suominen",
-      'author_email':"valueprivacy-foss@yahoo.com",
-      'maintainer':'Edwin A. Suominen',
-      'maintainer_email':"valueprivacy-foss@yahoo.com",
-      
-      'install_requires':required,
-      'packages':['logalyzer', 'logalyzer.test',],
-      'package_data':{
-          'mcmandelbrot': ['server-install.sh', 'mcm.*', 'blank.jpg'],
+      'url':               "http://edsuom.com/{}.html".format(NAME),
+      'project_urls':      {
+          'GitHub':     "https://github.com/edsuom/{}".format(NAME),
+          'API':        "http://edsuom.com/{}/{}.html".format(
+              NAME, NAME.lower()),
           },
-      'entry_points':{
+      'author':            "Edwin A. Suominen",
+      'author_email':      "foss@edsuom.com",
+      'maintainer':        "Edwin A. Suominen",
+      'maintainer_email':  "foss@edsuom.com",
+      
+      'install_requires':  required,
+      'packages':          [
+          'logalyzer', 'logalyzer.test',
+      ],
+      'package_data':      {
+          'mcmandelbrot': [
+              'server-install.sh', 'mcm.*', 'blank.jpg'
+          ],
+      },
+      'entry_points':      {
           'console_scripts': [
               'la = logalyzer.main:run',
           ],
       },
+      'test_suite':        "asynqueue.test",
 }
 
 kw['keywords'] = [
-    'Twisted', 'asynchronous', 'threads',
-    'taskqueue', 'queue', 'priority', 'tasks', 'jobs', 'nodes', 'cluster'
+    'twisted', 'asynchronous', 'async', 'threads',
+    'parallel', 'distributed',
+    'task', 'queue', 'priority', 'multicore', 'fractal',
 ]
 
 
@@ -71,24 +83,32 @@ kw['classifiers'] = [
     'License :: OSI Approved :: Apache Software License',
     'Operating System :: OS Independent',
     'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 2 :: Only',
     'Framework :: Twisted',
 
+    'Topic :: System :: Clustering',
     'Topic :: System :: Distributed Computing',
     'Topic :: Software Development :: Object Brokering',
     'Topic :: Software Development :: Libraries :: Python Modules',
 ]
 
-
-kw['description'] = " ".join("""
-
-Asynchronous task queueing based on the Twisted framework.
-""".split("\n"))
+# You get 77 characters. Use them wisely.
+kw['description'] =\
+"Asynchronous task queueing with Twisted: threaded, multicore, and remote."
 
 kw['long_description'] = """
 Asynchronous task queueing based on the Twisted framework, with task
 prioritization and a powerful worker interface. Worker implementations
 are included for running tasks asynchronously in the main thread, in
-separate threads, and in separate Python interpreters (multiprocessing).
+separate threads, in separate Python interpreters (multiprocessing),
+and even on separate devices using Twisted's Asynchronouse Message
+Protocol.
+
+Includes deferred iteration capability: Calling a task that returns an
+iterator can return a Deferator_ instead, which does the iteration in
+a Twisted-friendly fashion, even over a network connection. You can
+also supply an object conforming to Twisted's IConsumer interface and
+iterations will be fed to it as they become available.
 
 Includes an example package mcMandelbrot_ that generates Mandelbrot
 set images, row by row, demonstrating the power of asynchronous
@@ -104,6 +124,8 @@ Python 3 compatiblity is in the works, but not yet supported.
 .. _mcMandelbrot: http://edsuom.com/mcMandelbrot.html
 
 .. _ProcessQueue: http://edsuom.com/AsynQueue/asynqueue.process.ProcessQueue.html
+
+.. _Deferator: http://edsuom.com/AsynQueue/asynqueue.iteration.Deferator.html
 """
 
 ### Finally, run the setup
