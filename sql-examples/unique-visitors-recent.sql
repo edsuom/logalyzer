d5133d0c186405dd79371f998d225d25d9d2a99e
Edwin A. Suominen <valueprivacy-foss@yahoo.com>
2018-06-12 11:05:03 -0700

    Using the same edsuom.com site as the software is found on.

--- sql-examples/unique-visitors-recent.sql
+++ sql-examples/unique-visitors-recent.sql
@@ -1,7 +1,7 @@
 SELECT year(e.dt) YR, month(e.dt) MO, count(distinct e.id_url) N, e.ip IP
 FROM entries e INNER JOIN vhost ON e.id_vhost = vhost.id
 INNER JOIN url ON e.id_url = url.id
-WHERE vhost.value REGEXP '^(www\.)?tellectual\.com'
+WHERE vhost.value REGEXP '^(www\.)?edsuom\.com'
  AND e.http != 404
  AND url.value NOT REGEXP '\.(jpg|png|gif|ico|css)'
 GROUP BY IP, YR, MO
